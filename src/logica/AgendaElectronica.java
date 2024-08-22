540c18425d01699146fa0ff9e80f41edac2844d5
TomasBozzano <tomycas10@gmail.com>
2024-08-22 00:14:08 -0300

    Agenda electronica
    Manejo de Array con información

---
+++ src/logica/AgendaElectronica.java
@@ -0,0 +1,12 @@
+package logica;
+import gui.agendaGui;
+
+public class AgendaElectronica {
+
+    public static void main(String[] args) {
+         agendaGui agenda = new agendaGui();
+         agenda.setVisible(true);
+         agenda.setLocationRelativeTo(null);
+    }
+    
+}
